7e36e5a0df4f1cc3ad46ecd903c16da359d020f7
Poltorak Serguei <ps@z-wave.me>
2021-01-19 00:18:56 +0300

    Added build script and conversion in HTML

---
+++ build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# PDF version
+pdflatex ZWayManual.tex
+
+# Prepare HTML version
+
+rm -R ZWayManual
+
+latex2html -split +1 -no_auto_link -long_titles 10 -use_pdftex -html_version 5.0,math,unicode -no_navigation ZWayManual.tex
+
+cd ZWayManual
+
+# extract TOC
+echo '<div id="TOC">' > TOC.html
+awk '/^<!--End of Table of Child-Links-->$/ { tocStart = 0 } { if (tocStart) { print } } /^<!--Table of Child-Links-->$/ { tocStart = 1 }' ZWayManual.html >> TOC.html
+echo '</div>' >> TOC.html
+# delete TOC
+awk -i inplace '/^<!--Table of Child-Links-->$/ { tocStart = 1 } !/^<BR><HR>$/ { if (!tocStart) { print } } /^<!--End of Table of Child-Links-->$/ { tocStart = 0 }' ZWayManual.html
+# add CSS styles and alter existing
+sed -i 's/^\.CENTER  { text-align:center; }$/.CENTER  { }/' ZWayManual.css
+sed -i 's/^\.CENTER > \* { margin:auto; }$/.CENTER > * { }/' ZWayManual.css
+cat ../html/ZWayManualAddOn.css >> ZWayManual.css
+# copy JS for TOC
+cp ../html/ZWayManualAddOn.js ZWayManual.js
+
+for f in `ls -1 *.html`; do
+	# change meta keywords
+	sed -i 's/<META NAME="keywords" CONTENT="ZWayManual">/<META NAME="keywords" CONTENT="Z-Way Manual">/' $f
+	# change title
+	sed -i 's/<TITLE>/<TITLE>Z-Way Manual — /' $f
+	# delete subsection TOC
+	awk -i inplace '/^<!--Table of Child-Links-->$/ { tocStart = 1 } !/^<HR>$/ { if (tocStart == 2) { tocStart = 0 } } { if (!tocStart) { print } } /^<!--End of Table of Child-Links-->$/ { tocStart = 2 }' $f
+	# add TOC in new format
+	sed -i '/<BODY >/ r TOC.html' $f
+	# install JS for TOC
+	sed -i 's|</HEAD>|<script src="ZWayManual.js" defer></script>\n</HEAD>|' $f
+done
---
+++ html/ZWayManualAddOn.js
@@ -0,0 +1,70 @@
+ var link = location.href.substr(location.href.lastIndexOf('/') + 1);
+ var indx = link.indexOf('#');
+ var chapterName = indx > -1 ? link.substr(0, indx) : link;
+ var sectionName = indx > -1 ? link.substr(indx + 1) : "";
+
+ // delete unused br in the TOC
+ document.querySelectorAll('#TOC > ul > li > br').forEach(function(br) { br.remove(); });
+ // delete link to TOC from TOC
+ document.querySelector('#TOC > ul > li a[href="Contents.html"]').parentElement.remove()
+ 
+ // collapse all chapters but current 
+ document.querySelectorAll('#TOC > ul > li').forEach(function(n) {
+   if (n.querySelector('a').href.indexOf('/' + chapterName) === -1 ) {
+     var ul = n.querySelector('ul');
+     if (ul) {
+       ul.style.display='none'
+     }
+   }
+ });
+ 
+ // mark in bold current item and scroll in the center
+ function hiliteTOCElement(section) {
+   var oldItem = document.querySelector('#TOC ul li a.tocCurrentItem');
+   if (oldItem) oldItem.classList.remove("tocCurrentItem");
+   
+   var curItem = document.querySelector('#TOC ul li a[href="' + chapterName + '#' + section + '"]');
+   if (curItem) {
+     curItem.classList.add("tocCurrentItem");
+     curItem.scrollIntoView({block: 'center'});
+   }
+   
+   if (history.pushState) {
+     history.pushState(null, null, '#' + section);
+   }
+ }
+
+ hiliteTOCElement(sectionName);
+  
+ // find absolute position of an element
+ function absTop(element) {
+    var top = 0;
+    do {
+        top += element.offsetTop  || 0;
+        element = element.offsetParent;
+    } while(element);
+
+    return top;
+ }
+ 
+ var anchorPositions = {};
+ document.querySelectorAll('a[id]').forEach(function(a) {
+   // the link is SECTION and there is a reference in TOC
+   if (a.id.startsWith("SECTION") && document.querySelector('#TOC > ul > li a[href="' + chapterName + '#' + a.id + '"]')) {
+     anchorPositions[a.id] = absTop(a);
+   }
+ });
+ 
+ document.addEventListener("scroll", function(){
+   var top = document.documentElement.scrollTop + 10; // +10 to make sure the same section is active when we click on it
+   
+   var dist = null;
+   var closest = "";
+   Object.keys(anchorPositions).forEach(function(id) {
+     if (dist === null || (0 < top - anchorPositions[id] && top - anchorPositions[id] < dist)) {
+       dist = top - anchorPositions[id];
+       closest = id;
+     }
+   });
+   hiliteTOCElement(closest);
+ });
